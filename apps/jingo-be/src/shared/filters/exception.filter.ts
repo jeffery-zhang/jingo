9598c380052c4f48f7d938a6731aebf64146fbfd
jeffery-zhang <jefferyzhang1013@gmail.com>
2023-07-17 18:17:04 +0800

    fix:fixing exception catcher bug

--- apps/jingo-be/src/shared/filters/exception.filter.ts
+++ apps/jingo-be/src/shared/filters/exception.filter.ts
@@ -9,13 +9,21 @@ import { Response } from 'express'
 @Catch(HttpException)
 export class GlobalExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
+    console.log('lets see exception response: ', exception.getResponse())
     const ctx = host.switchToHttp()
     const response = ctx.getResponse<Response>()
     const status = exception.getStatus()
 
+    const getExceptionMessage = () => {
+      if (exception.message) return exception.message
+      const res = exception.getResponse()
+      if (typeof res === 'string') return res
+      if (typeof res === 'object') return (res as any).message
+    }
+
     response.status(status).json({
       success: false,
-      message: exception.message,
+      message: getExceptionMessage(),
       data: [],
       status,
     })
